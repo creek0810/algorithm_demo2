c15fce331f270697f7d50602559367789bd87ebb
creek0810 <creek880810@gmail.com>
2018-10-06 22:43:42 +0800

    add the color feature

--- bubble_sort/bubble_sort.js
+++ bubble_sort/bubble_sort.js
@@ -1,4 +1,5 @@
 // TODO: clean the code
+// TODO: adjust timeline
 var data = [{"id": "#data0","value": 5,"loc": 0},
             {"id": "#data1","value": 2,"loc": 0},
             {"id": "#data2","value": 7,"loc": 0},
@@ -18,9 +19,7 @@ function swapright(target) {
   var des = pos+51;
   data[target]["loc"]=des;
   console.log("right: "+des+" "+data[target]["value"]);
-
   var id = setInterval(frame, 5);
-
   function frame() {
     if (pos == des) {
       clearInterval(id);
@@ -48,30 +47,55 @@ function swapleft(target) {
   }
   console.log("final left: "+$(data[target]["id"]).css("left"));
 }
-function call(k) {
+function cmp(k) {
     return new Promise(function (resolve, reject) {
+        $(data[k]["id"]).toggleClass("rec",false);
+        $(data[k-1]["id"]).toggleClass("rec",false);
+        $(data[k]["id"]).toggleClass("rec_cmp",true);
+        $(data[k-1]["id"]).toggleClass("rec_cmp",true);
         if(data[k]["value"]<data[k-1]["value"]){
-            swapright(k-1);
-            swapleft(k);
+            setTimeout(function(){
+              swapright(k-1);
+              swapleft(k);
+            },500);
             setTimeout(function(){
                 var tmp=data[k];
                 data[k]=data[k-1];
                 data[k-1]=tmp;
+                $(data[k]["id"]).toggleClass("rec_cmp",false);
+                $(data[k-1]["id"]).toggleClass("rec_cmp",false);
+                $(data[k]["id"]).toggleClass("rec",true);
+                $(data[k-1]["id"]).toggleClass("rec",true);
+                resolve();
+            },1500)
+        }else{
+            setTimeout(function(){
+                $(data[k]["id"]).toggleClass("rec_cmp",false);
+                $(data[k-1]["id"]).toggleClass("rec_cmp",false);
+                $(data[k]["id"]).toggleClass("rec",true);
+                $(data[k-1]["id"]).toggleClass("rec",true);
                 resolve();
             },1000)
-        }else{
-            resolve();
         }
     })
 };
-
+function wait(k) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+        resolve();
+        },k);
+    })
+};
 $(document).ready(function(){
     $("#start").click(async function(){
         var count=0;
         for(i=0;i<data.length;i++){
+            await wait(1000);
             for(k=1;k<data.length-i;k++){
-                await call(k);
+                await cmp(k);
             }
+            $(data[data.length-i-1]["id"]).toggleClass("rec",false);
+            $(data[data.length-i-1]["id"]).toggleClass("rec_finish",true);
         }
     })
 });
