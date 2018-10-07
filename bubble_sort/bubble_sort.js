ec52d1cc224a091ec4116dc1769d48051991bfbe
creek0810 <creek880810@gmail.com>
2018-10-07 11:13:20 +0800

    clean code~

--- bubble_sort/bubble_sort.js
+++ bubble_sort/bubble_sort.js
@@ -1,62 +1,55 @@
-// TODO: clean the code
-// TODO: adjust timeline
+// TODO: [x]clean the code
+// TODO: [X] adjust timeline
+
+// the data of current state
 var data = [{"id": "#data0","value": 5,"loc": 0},
             {"id": "#data1","value": 2,"loc": 0},
             {"id": "#data2","value": 7,"loc": 0},
             {"id": "#data3","value": 3,"loc": 0},
             {"id": "#data4","value": 9,"loc": 0},
-            {"id": "#data5","value": 1,"loc": 0}];
-$(document).ready(function(){
-    for(var i=0;i<data.length;i++){
-        var target = data[i]["id"];
-        $(target).html(data[i]["value"].toString());
-        $(target).css("line-height",(data[i]["value"]*34-15)+"px");
-        $(target).height(data[i]["value"]*17);
-    }
-});
-function swapright(target) {
+            {"id": "#data5","value": 1,"loc": 0},
+            {"id": "#data6","value": 4,"loc": 0}];
+// const data
+var Data = [{"id": "#data0","value": 5,"loc": 0},
+            {"id": "#data1","value": 2,"loc": 0},
+            {"id": "#data2","value": 7,"loc": 0},
+            {"id": "#data3","value": 3,"loc": 0},
+            {"id": "#data4","value": 9,"loc": 0},
+            {"id": "#data5","value": 1,"loc": 0},
+            {"id": "#data6","value": 4,"loc": 0}];
+// to store if it is the first time that the program run
+var flag=true;
+// move animate
+function move(target,dis) {
   var pos = data[target]["loc"];
-  var des = pos+51;
+  var des = pos+dis;
   data[target]["loc"]=des;
-  console.log("right: "+des+" "+data[target]["value"]);
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 2);
   function frame() {
     if (pos == des) {
       clearInterval(id);
     } else {
-      pos++;
+      if(pos>des){
+          pos--;
+      }else{
+          pos++;
+      }
       $(data[target]["id"]).css("left",pos+"px");
     }
   }
-  console.log("final right: "+$(data[target]["id"]).css("left"));
-
-}
-function swapleft(target) {
-  var pos = data[target]["loc"];
-  var des = pos-51;
-  data[target]["loc"]=des;
-  console.log("left: "+des+" "+data[target]["value"]);
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == des) {
-      clearInterval(id);
-    } else {
-      pos--;
-      $(data[target]["id"]).css("left",pos+"px");
-    }
-  }
-  console.log("final left: "+$(data[target]["id"]).css("left"));
 }
 function cmp(k) {
     return new Promise(function (resolve, reject) {
+        // change color to comparing mode
         $(data[k]["id"]).toggleClass("rec",false);
         $(data[k-1]["id"]).toggleClass("rec",false);
         $(data[k]["id"]).toggleClass("rec_cmp",true);
         $(data[k-1]["id"]).toggleClass("rec_cmp",true);
         if(data[k]["value"]<data[k-1]["value"]){
+            // swap
             setTimeout(function(){
-              swapright(k-1);
-              swapleft(k);
+              move(k-1,51);
+              move(k,-51);
             },500);
             setTimeout(function(){
                 var tmp=data[k];
@@ -67,8 +60,9 @@ function cmp(k) {
                 $(data[k]["id"]).toggleClass("rec",true);
                 $(data[k-1]["id"]).toggleClass("rec",true);
                 resolve();
-            },1500)
+            },1000)
         }else{
+            // don't need to swap
             setTimeout(function(){
                 $(data[k]["id"]).toggleClass("rec_cmp",false);
                 $(data[k-1]["id"]).toggleClass("rec_cmp",false);
@@ -87,15 +81,33 @@ function wait(k) {
     })
 };
 $(document).ready(function(){
+    // init graph
+    for(var i=0;i<data.length;i++){
+        var target = data[i]["id"];
+        $(target).html(data[i]["value"].toString());
+        $(target).css("line-height",(data[i]["value"]*34-15)+"px");
+        $(target).height(data[i]["value"]*17);
+    }
     $("#start").click(async function(){
-        var count=0;
+        // check if graph need to reset
+        if(flag==false){
+            for(var i=0;i<Data.length;i++){
+                var target = Data[i]["id"];
+                $(target).css("left",0+"px");
+                $(data[data.length-i-1]["id"]).toggleClass("rec_finish",false);
+                $(data[data.length-i-1]["id"]).toggleClass("rec",true);
+            }
+            data=Data;
+        }
+        // bubble sort
         for(i=0;i<data.length;i++){
-            await wait(1000);
+            await wait(500);
             for(k=1;k<data.length-i;k++){
                 await cmp(k);
             }
             $(data[data.length-i-1]["id"]).toggleClass("rec",false);
             $(data[data.length-i-1]["id"]).toggleClass("rec_finish",true);
         }
+        flag=false;
     })
 });
