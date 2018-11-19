4c3c5daac89e9e7075be7c9bcbe5050c40ecd220
creek0810 <creek880810@gmail.com>
2018-11-19 17:31:09 +0800

    fixed stack rwd problem

--- data_structure/stack.js
+++ data_structure/stack.js
@@ -1,6 +1,9 @@
 var stack_top = 0;
 var easter_egg_try = 0
+var LEFT = 0;
 function stack_push(){
+    var myClient = $('.data')[0].getBoundingClientRect();
+    LEFT = myClient.left + 15; 
     var fallen_time = 0;
     var data = $("#num").val();
     var anime_node = "<div class=\"anime_rec\">" + data.toString() + "</div>";
@@ -10,18 +13,24 @@ function stack_push(){
         targets: '#pic',
         duration: 2000,
         easing: 'linear',
-        translateX: -1450,
+        translateX: function(){
+            return LEFT - 1800 ;
+        },
     }).add({
         targets: '.anime_rec',
         duration: 2000,
         offset: "-=2000",
         easing: 'linear',
-        translateX: -1461,
+        translateX: function(){
+            return LEFT - 1800;
+        } 
     }).add({
         targets: '.anime_rec',
         duration: 1500,
         offset: 2000,
-        translateX: -1461,
+        translateX: function(){
+            return LEFT - 1800;
+        },
         translateY: 379 - (32)*stack_top,
         easing: 'linear',
         complete: function(){
@@ -72,6 +81,35 @@ function info_change(mode){
     $("."+option[(mode+1)%2]).css("display", "none");
     console.log(mode);
 }
+function getPosition (element) {
+  console.log(element);
+  var x = 0;
+  var y = 0;
+  // 搭配上面的示意圖可比較輕鬆理解為何要這麼計算
+  while ( element ) {
+  console.log(element);
+    x += element.offsetLeft - element.scrollLeft + element.clientLeft;
+    y += element.offsetTop - element.scrollLeft + element.clientTop;
+    element = element.offsetParent;
+  }
+  console.log(x);
+  return { x: x, y: y };
+}
+function getElementLeft(element){
+　　　　var actualLeft = element.offsetLeft;
+　　　　var current = element.offsetParent;
+console.log(actualLeft);
+
+　　　　while (current !== null){
+    
+        console.log(current);
+　　　　　　actualLeft += current.offsetLeft;
+　　　　　　current = current.offsetParent;
+　　　　}
+
+        console.log(actualLeft);
+　　　　return actualLeft;
+　　}
 function init(){
     $("#push").click(stack_push);
     $("#top").click(stack_top);
@@ -84,6 +122,5 @@ function init(){
     $("#trace").click(function(){
         info_change(1);
     });
-    
 }
 $(document).ready(init,false);
