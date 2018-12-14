f34b4a4d960bb11a122fdce760748728f633b56c
creek0810 <creek880810@gmail.com>
2018-12-14 23:33:02 +0800

    add feature of queue var and speed

--- data_structure/queue.js
+++ data_structure/queue.js
@@ -1,25 +1,62 @@
 var stack_top = 0;
 var easter_egg_try = 0
 var LEFT = 0;
+var rear = 0;
+var front = 0;
+var capacity = 11;
+function content_init(){
+    var pop_code = "int queue[11] = {0};\n" + 
+                   "int front = 0, rear = 0, capacity = 11;\n" +
+                   "void pop(int queue[]){\n" +
+                   "    if(front == rear){\n" + 
+                   "        printf(\"Queue is empty\");\n" +
+                   "    }else{\n" +
+                   "        font++;\n" +
+                   "    }\n" +
+                   "}\n";
+    var push_code = "int queue[11] = {0};\n" + 
+                    "int front = 0, rear = 0, capacity = 11;\n" +
+                    "void push(int queue[], int data){\n" + 
+                    "    if((rear+1)%capacity == front){\n" + 
+                    "        printf(\"Queue is full\");\n" + 
+                    "    }else{\n" + 
+                    "        rear = (rear + 1) % capacity;\n" + 
+                    "        queue[rear] = data;\n" + 
+                    "    }\n" + 
+                    "}\n";
+    $(".pop_inner").html(pop_code);
+    $(".push_inner").html(push_code);
+    $(".code_pop").hide();
+    $(".code_push").hide();
+    hljs.initHighlighting();
+}
 function content_change(command){
     // command 0 = push
     // command 1 = pop
     if(command == 1){
-        var code = '<h3>Code</h3>' +
-        'int queue[11] = {0};<br>int front = 0, rear = 0, capacity = 11;<br>void pop(int queue[]){<br><tab1>if(front == rear){</tab1><tab2>printf("Queue is empty");</tab2><tab1>}else{</tab1><tab2>front++;</tab2><tab1>}</tab1>}';
+        $(".code_pop").show();
+        $(".code_push").hide();
         var info = "Function: Pop<br>" +
                    "Time complexity: O(1)<br>";
     }else{
-        var code = '<h3>Code</h3>int queue[11] = {0};<br>int front = 0, rear = 0, capacity = 11;<br>void push(int queue[], int data){<br><tab1>if((front+1)%capacity == rear){</tab1><tab2>printf("Queue is full");</tab2><tab1>}else{</tab1><tab2>rear = (rear + 1) % capacity;</tab2><tab2>queue[rear] = data;</tab2><tab1>}</tab1>}'
+        $(".code_push").show();
+        $(".code_pop").hide();
         var info = "Function: Push<br>" +
                    "Time complexity: O(1)<br>";
     }
-    $("#code").html(code);
     $(".info").html(info);
 }
-function stack_push(){
+function queue_push(){
+    // check if the queue is full
+    if((rear+1)%capacity == front){
+        alert("The queue is full");
+        return;
+    }
+    // store speed
+    var dur = $("#speed").val();
     // change the content of code trace
     content_change(0);
+    info_change(1);
     // add helicopter
     var heli_node = '<div id="pic"><iframe src="helicopter.svg" frameborder="0" class="heli"></iframe></div>';
     $("body").prepend(heli_node);
@@ -33,8 +70,10 @@ function stack_push(){
     // start anime
     var timeline = anime.timeline();
     timeline.add({
-        targets: '#pic',
-        duration: 2000,
+        targets: ['#pic', '.anime_rec'],
+        duration: function(){
+            return Math.floor(2000 / dur);
+        },
         easing: 'linear',
         translateX: function(){
             return LEFT - 1800 ;
@@ -45,16 +84,12 @@ function stack_push(){
         }
     }).add({
         targets: '.anime_rec',
-        duration: 2000,
-        offset: "-=2000",
-        easing: 'linear',
-        translateX: function(){
-            return LEFT - 1800;
-        } 
-    }).add({
-        targets: '.anime_rec',
-        duration: 1500,
-        offset: 2000,
+        duration: function(){
+            return Math.floor((379 - (32)* stack_top) * 5 / dur);
+        },
+        delay: function(){
+            return Math.floor(400 / dur);
+        },
         translateX: function(){
             return LEFT - 1800;
         },
@@ -68,7 +103,9 @@ function stack_push(){
         }
     }).add({
         targets: '#pic',
-        duration: 600,
+        duration: function(){
+            return Math.floor(600 / dur);
+        },
         easing: 'linear',
         translateX: -2000,
         complete: function(){
@@ -76,16 +113,26 @@ function stack_push(){
             $("#push").attr("disabled",false);
             $("#pop").attr("disabled",false);
         }
+    }).add({
+        targets: '#var_rear',
+        value: function(){
+            rear = (rear + 1) % capacity;
+            return rear;
+        },
+        duration: 1,
+        round: 1
     });
 }
-function stack_top(){
 
-}
-function stack_pop(){
+
+function queue_pop(){
+    // store speed
+    var dur = $("#speed").val();
     // change the content of code trace
     content_change(1);
+    info_change(1);
     if(stack_top == 0){
-        alert("the stack is empty!!");
+        alert("the queue is empty!!");
         easter_egg_try++;
     }else{
         // add mouse
@@ -104,9 +151,12 @@ function stack_pop(){
         var rec_final_y = 130 - $('.anime_rec_pop')[0].getBoundingClientRect().top;
         var mouse_des = rec_pos.left - 1755;
         var timeline = anime.timeline();
+        var tmp_offset = "-=" + Math.floor(2000/dur).toString(); 
         timeline.add({
             targets: '#pic_mouse',
-            duration: 2000,
+            duration: function(){
+                return Math.floor(2000 / dur);
+            },
             easing: 'linear',
             translateX: mouse_des,
             bagin: function(){
@@ -114,9 +164,13 @@ function stack_pop(){
                 $("#pop").attr("disabled", true);
             }
         }).add({
-            delay: 500,
+            delay: function(){
+                return Math.floor(500 / dur);
+            },
             targets: '#pic_mouse',
-            duration: 292,
+            duration: function(){
+                return Math.floor(292 / dur);
+            },
             translateX: function(){
                 return mouse_des - 117;
             },
@@ -125,29 +179,36 @@ function stack_pop(){
                 $(".rec:last").remove();
             }
         }).add({
-            targets: '.anime_rec_pop',
-            duration: 2000,
-            offset: "-=292",
-            translateX: -800,
-            easing: 'linear',
-        }).add({
             targets: '#pic_mouse',
             easing: 'linear',
-            duration: 1708,
-            offset: "-=1708",
+            duration: function(){
+                return Math.floor(1708 / dur);
+            },
             translateX: function(){
                 return mouse_des - 800;
             },
-        }).add({
-            targets: "#pic_mouse",
-            easing: "linear",
-            duration: 1,
             complete: function(){
                 $("#pic_mouse").remove();
                 $(".anime_rec_pop").remove();
                 $("#push").attr("disabled", false);
                 $("#pop").attr("disabled", false);
             }
+        }).add({
+            targets: '.anime_rec_pop',
+            duration: function(){
+                return Math.floor(2000 / dur);
+            },
+            offset: tmp_offset,
+            translateX: -800,
+            easing: 'linear',
+        }).add({
+            targets: '#var_front',
+            value: function(){
+                front++;
+                return front;
+            },
+            duration: 1,
+            round: 1
         });
         stack_top--;
     }
@@ -171,9 +232,9 @@ function info_change(mode){
     $("."+option[(mode+1)%2]).css("display", "none");
 }
 function init(){
-    $("#push").click(stack_push);
-    $("#top").click(stack_top);
-    $("#pop").click(stack_pop);
+    content_init();
+    $("#push").click(queue_push);
+    $("#pop").click(queue_pop);
     // mode 0 = info
     // mode 1 = trace
     $("#info").click(function(){
