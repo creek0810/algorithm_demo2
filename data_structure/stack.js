c8c3827af3d8f4fe3837c7d1e64223063e1a5ecd
creek0810 <creek880810@gmail.com>
2018-12-27 17:45:03 +0800

    disable the push  btn

--- data_structure/stack.js
+++ data_structure/stack.js
@@ -173,8 +173,10 @@ function corn_push(){
             $("#pic").remove();
             $("#push").attr("disabled",false);
             $("#pop").attr("disabled",false);
-            $('#modal-content').text("OK! You got corn.");
+            $('#modal-content').html("<b>OK! You got corn.<br><span style='color:#0092ca'>Please pop again.</span></b>");
             $('#myModal').modal('toggle');
+            $('#pop').addClass('btn_special');
+            $('#push').attr('disabled',true);
         }
     });
 }
@@ -219,15 +221,13 @@ function pop_corn(){
         complete: function(){
             $('#modal-content').html("<b>You <span style='color: red'>pop corn </span>and you got <span style='color: red'>POP-CORN! </span>HAHAHA!<br> <span style='color:#0092ca'>Please refresh the website!</span></b>");
             $('#myModal').modal('toggle');
-//            alert("Please refresh the website!");
         }
     });
 }
 function stack_pop(){
     if(stack_top == 0){
         let msg = ["已經沒有東西可以pop了!", "看來你真的很想pop?", "認真的?"];
-        console.log(easter_egg_try);
-        $('#modal-content').text(msg[easter_egg_try]);
+        $('#modal-content').html('<b>' + msg[easter_egg_try] + '</b>');
         if(easter_egg_try == 3){
             corn_push();
         }else if(easter_egg_try == 4){
